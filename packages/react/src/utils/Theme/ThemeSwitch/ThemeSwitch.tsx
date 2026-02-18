cefb8d8dd2e794509dc8f17cf1e210f72d3e0975
charles olivier <chla84@yahoo.fr>
2026-02-18 11:26:26 +0100

    add ThemeSwitch contrast styles, refactor header menu dark/light link styles (v0.0.49)

--- packages/react/src/utils/Theme/ThemeSwitch/ThemeSwitch.tsx
+++ packages/react/src/utils/Theme/ThemeSwitch/ThemeSwitch.tsx
@@ -1,5 +1,7 @@
+import clsx from 'clsx'
 import { Switch } from '../../../components/form/Switch/Switch'
 import { useTheme } from '../ThemeContext'
+import styles from './ThemeSwitch.module.scss'
 import type { ThemeSwitchTypes } from './ThemeSwitch.types'
 
 export function ThemeSwitch({
@@ -7,11 +9,13 @@ export function ThemeSwitch({
   ...rest
 }: ThemeSwitchTypes) {
   const { theme, setTheme } = useTheme()
+  const isDark = theme === 'dark'
 
   return (
     <Switch
       label={label}
-      checked={theme === 'dark'}
+      labelClassName={clsx(styles.label, isDark ? styles.contrast : '')}
+      checked={isDark}
       onChange={(checked) => setTheme(checked ? 'dark' : 'light')}
       {...rest}
     />
